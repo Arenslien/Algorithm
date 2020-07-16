f8c519110da3885fed9fdf3355e90fa1703be673
Arenslien <ibo703@naver.com>
2020-07-16 23:11:32 +0900

    linked list

---
+++ linked_list1.py
@@ -0,0 +1,17 @@
+class Linked_list:
+    class Node:
+        def __init__(self, value = None, front = None, back = None):
+            self.value = value
+            self.front = front
+            self.back = back
+    def __init__(self):
+        
+        pass
+
+    def add(self, value):
+
+
+test = Linked_list()
+print()
+        
+    
